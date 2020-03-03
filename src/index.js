f849e859b40fde043b2013f430a5ebc4e0f4bc9a
Vitali-Hrybouski <60929162+Vitali-Hrybouski@users.noreply.github.com>
2020-03-03 07:58:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const n = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety",
+        100: "hundred"
+    }
+    let strnumber = number.toString();
+    let result = [];
+
+    if (strnumber.length === 3) {
+        result.push(n[strnumber[0]]);
+        result.push(n["100"]);
+        if (number % 100 != 0) {
+            number = number % 100;
+            strnumber = number.toString();
+        }
+    }
+
+    if (number >= 20 && strnumber.length == 2) {
+        result.push(n[strnumber[0] + "0"]);
+        if (number % 10 !== 0) {
+            result.push(n[strnumber[1]]);
+        }
+    }
+    if (number < 20 || strnumber.length == 1) {
+        result.push(n[number]);
+
+    }
+    return result.join(" ");
+}  
